2caea9508cd50902dcc41e682c9b3c2b74abee2a
Dhohir Pradana <dhohirkudus@gmail.com>
2023-10-24 12:52:56 +0700

    feat: dynamic kernel

--- execute/__init__.py
+++ execute/__init__.py
@@ -6,63 +6,86 @@ import requests
 import asyncio
 from flask import jsonify
 import os
+import threading
 
 jupyterhub_url = os.environ.get('JUPYTERHUB_URL')
 jupyterhub_ws = os.environ.get('JUPYTERHUB_WS')
 token = os.environ.get('JUPYTERHUB_TOKEN')
-kernel = os.environ.get('JUPYTERHUB_KERNEL')
 
 api_url = f"{jupyterhub_url}/hub/api"
 
-async def execute(index, username, cell_source):
+
+async def execute_ws(index, username, cell_source, kernel):
     uuid4 = uuid.uuid4()
+    msg_id = uuid.uuid4()
     now = datetime.now()
     formatted_date = now.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
     uri = f"{jupyterhub_ws}/user/{username}/api/kernels/{kernel}/channels?session_id={uuid4}&token={token}"
     print("uri", uri)
 
+    message = {
+        "header": {
+            "date": formatted_date,
+            "msg_id": f"{msg_id}",
+            "msg_type": "execute_request",
+            "session": f"{uuid4}",
+            "username": "",
+            "version": "5.2"
+        },
+        "parent_header": {},
+        "metadata": {
+            "editable": True,
+            "slideshow": {
+                "slide_type": ""
+            },
+            "tags": [],
+            "trusted": True,
+            "deletedCells": [],
+            "recordTiming": False
+        },
+        "content": {
+            "code": cell_source,
+            "silent": False,
+            "store_history": True,
+            "user_expressions": {},
+            "allow_stdin": True,
+            "stop_on_error": True
+        },
+        "buffers": []
+    }
+
+    # print(message)
+    message_json = json.dumps(message)
+    # print(message_json)
+
     async with websockets.connect(uri) as websocket:
-        message = {
-            "header": {
-                "date": f"{formatted_date}",
-                "msg_id": f"{index}",
-                "msg_type": "scheduled_execute",
-                "session": f"{uuid4}",
-                "username": f"{username}",
-                "version": "5.2"
-            },
-            "parent_header": {},
-            "metadata": {
-                "editable": True,
-                "slideshow": {
-                    "slide_type": ""
-                },
-                "tags": [],
-                "trusted": True,
-                "deletedCells": [],
-                "recordTiming": False
-            },
-            "content": {
-                "code": f"{cell_source}",
-                "silent": False,
-                "store_history": True,
-                "user_expressions": {},
-                "allow_stdin": True,
-                "stop_on_error": False
-            },
-            "buffers": []
-        }
-
-        message_json = json.dumps(message)
         await websocket.send(message_json)
+        # print(f"Sent: {message_json}")
 
-        # Receive a response if needed
-        response = await websocket.recv()
+        # Subscribe to incoming messages
+        while True:
+            response = await websocket.recv()
+            response_json = json.loads(response)
+            content = response_json['content']
+            msg_state = response_json["header"]["msg_type"]
+            # print("Msg type:", msg_state)
+            # print("Response", response)
+            # print("Content", content)
 
-        print(f"Received response: {response}")
+            if msg_state == 'input_request':
+                return {'status': 'error', 'msg': 'input promt:'}
 
-# asyncio.get_event_loop().run_until_complete(handler())
+            if msg_state == 'error':
+                err_msg = content['traceback']
+                # print(err_msg)
+                return {'status': 'error', 'msg': err_msg}
+
+            if 'status' in content:
+                # print(content['status'])
+                status = content['status']
+                return {'status': status, 'msg': content['traceback'] if status == 'error' else 'ok'}
+                # asyncio.get_event_loop().run_until_complete(handler())
 
 
 def handler(request):
@@ -75,23 +98,20 @@ def handler(request):
     if token is None:
         return jsonify({"message": "JUPYTERHUB_TOKEN is required!"})
 
-    if kernel is None:
-        return jsonify({"message": "JUPYTERHUB_KERNEL is required!"})
-
     body = request.get_json()
-    
+
     if body is None:
         return jsonify({"message": "Request body is required!"}), 400
 
     if "user" not in body:
         return jsonify({"message": "user is required!"}), 400
-    
+
     if "notebook-name" not in body:
         return jsonify({"message": "notebook-name is required!"}), 400
-    
+
     user = body["user"]
     notebook_name = body["notebook-name"]
-    
+
     if user is None:
         return jsonify({"message": "user is required!"}), 400
 
@@ -126,25 +146,61 @@ def handler(request):
                 response = r.json()
                 # print(response)
 
+                now = datetime.now()
+                formatted_date = now.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+                try:
+                    kernels_uri = f"{jupyterhub_url}/user/{user}/api/sessions?{formatted_date}"
+                    print(kernels_uri)
+
+                    rr = requests.get(kernels_uri, headers={
+                        'Authorization': f'token {token}'}, json={})
+                    rr.raise_for_status()
+
+                    responser = rr.json()
+                    print(responser)
+
+                    kernel_ids = [item["kernel"]["id"]
+                                  for item in responser if item["path"] == notebook_name]
+                    print(kernel_ids)
+
+                    if not len(kernel_ids):
+                        return jsonify({"message": "Unable get sessions!, empty!"}), 400
+                    else:
+                        kernel = kernel_ids[0]
+
+                except Exception as e:
+                    return jsonify({"message": "Unable get sessions!"}), 400
+
                 cells = response['content']['cells']
                 # print(cells)
+
+                results = []
 
                 for index, cell in enumerate(cells):
                     cell_source = cell['source']
                     cell_type = cell['cell_type']
-                    
-                    print("cell_type", cell_type)
+
+                    # print("cell_type", cell_type)
 
                     if cell_type == "code":
-                        async def execute():
-                            await execute(index, user, cell_source)
+                        # print(cell_source)
 
-                        loop = asyncio.get_event_loop()
-                        loop.run_until_complete(execute())
+                        async def abc():
+                            res = await execute_ws(index, user, cell_source, kernel)
+                            print(res)
+                            results.append({'cell': index + 1, **res})
+                        asyncio.run(abc())
                     else:
-                        pass
+                        if index+1 != len(cells):
+                            pass
+                        else:
+                            return jsonify({"message": "Finished", "total": len(cells), "results": results}), 200
 
-                return jsonify({"message": "Finished"}), 200
+                    if results[-1]['status'] == 'error':
+                        break
+
+                return jsonify({"message": "Finished", "total": len(cells), "results": results}), 200
 
             except Exception as e:
                 print(str(e))
