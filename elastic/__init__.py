b0cbe29473e01a4cf051349e4047e9cff63b3a4a
Dhohir Pradana <dhohirkudus@gmail.com>
2024-01-15 17:13:07 +0700

    update elastic/__init__.py

--- elastic/__init__.py
+++ elastic/__init__.py
@@ -14,6 +14,24 @@ def handler(data):
     document_id = uuid4
 
     try:
+        # Define the index mapping
+        index_mapping = {
+            "mappings": {
+                "properties": {
+                    "lastRun": {
+                        "type": "date",
+                        "format": "yyyy-MM-dd HH:mm:ss.SSSSSS"
+                    },
+                    "nextRun": {
+                        "type": "date",
+                        "format": "yyyy-MM-dd HH:mm:ss.SSSSSS"
+                    },
+                }
+            }
+        }
+
+        es.indices.create(index=index_name, body=index_mapping)
+
         es = Elasticsearch(hosts=elastic_url)
         es.index(index=index_name, document=data, id=document_id)
         print("Success insert data to elastic")
