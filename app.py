cac2b1b267aaa1dea402b800efdb25ccb0cec97b
Dhohir Pradana <dhohirkudus@gmail.com>
2024-02-13 10:31:20 +0700

    update app.py

--- app.py
+++ app.py
@@ -8,12 +8,7 @@ from pb_token import token_get as token_handler
 
 # List of required environment variables
 required_env_vars = ["JUPYTER_URL",
-<<<<<<< HEAD
                      "JUPYTER_WS", "JUPYTER_TOKEN", "ELASTIC_URL", "PB_LOGIN_URL", "PB_MAIL", "PB_PASSWORD", "PB_SCHEDULER_URL", "EVENT_URL"]
-=======
-                     "JUPYTER_WS", "ELASTIC_URL", "PB_LOGIN_URL", "PB_MAIL", "PB_PASSWORD", "PB_SCHEDULER_URL", "EVENT_URL", "JUPYTER_TOKEN"]
-# raise EnvironmentError(f"Required environment variable {env_var} is not set.")
->>>>>>> 872810911f6efb3ff5f1cc6769cbb888bf22f1a4
 
 def validate_envs():
     for env_var in required_env_vars:
